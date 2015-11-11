8413d9be810fd92b31271d1a0599d16cd5a1a8ed
trishnaguha <trishnaguha17@gmail.com>
2015-11-11 09:54:28 +0530

    Adding bind_test

--- nongatingtests.py
+++ nongatingtests.py
@@ -1,14 +1,20 @@
 import unittest
 import re
 from .testutils import system
-    
+
 class TunirNonGatingtests(unittest.TestCase):
-    
+
     def test_bash(self):
         """Tests the bash version as the same of upstream"""
         out, err, eid = system('bash --version')
         out = out.decode('utf-8')
         self.assertIn("-redhat-linux-gnu", out, out)
+
+    def test_bind(self):
+        """Tests bind: local resolver can qualify 127.0.0.1"""
+        out, err, eid = system('dig +timeout=1 +short 127.0.0.1 localhost')
+        out = out.decode('utf-8')
+        self.assertIn("127.0.0.1", out, out)
 
 if __name__ == '__main__':
     unittest.main()
